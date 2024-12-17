65b290e428cc4b89fa00668dfe65f250d6139106
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-12-17 18:05:54 -0400

    feat: criando o controller de usuarios, select, insert, delete, update

--- server/infra/controllers/users_controller.py
+++ server/infra/controllers/users_controller.py
@@ -7,13 +7,23 @@ from configs.connection import DBconnection
 from models.users_model import Users
 
 class UsersResponse:
-    def select(self, id):
+    def select(self):
         with DBconnection() as db:
-            data = db.session.query(Users).filter(Users.ID == id)
-            return data
+            select_data = db.session.query(Users).all()
+            return select_data
         
-    # def insert(self, nome, password):
-    #     with DBconnection() as db:
-    #         print('testandoo')
-    #         insert_data = db.session.insert(Users).values(NAME = nome, PASSWORD = password)
-    #         return insert_data
+    def insert(self, nome, password, id):
+        with DBconnection() as db:
+            insert_data = Users(NAME = nome, PASSWORD = password, ID = id)
+            db.session.add(insert_data)
+            db.session.commit()
+
+    def update(self, nome, password, id):
+        with DBconnection() as db:
+            db.session.query(Users).filter(Users.ID == id).update({"NAME": nome, "PASSWORD": password})
+            db.session.commit()
+
+    def delete(self, id):
+            with DBconnection() as db:
+                db.session.query(Users).filter(Users.ID == id).delete()
+                db.session.commit()
