c741b246f46ee288b6e7429aacb2f1c775883fad
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-12-18 13:17:04 -0400

    feat: adcionando o blueprint para as rotas de lojas

--- server/app.py
+++ server/app.py
@@ -1,15 +1,19 @@
 from flask import Flask
+from flask_pydantic_spec import FlaskPydanticSpec # Cria um endpoint chamado /doc/swagger que mostra os endpoints que tenho na minha aplicação.
 from routes.users_route import users_bp
+from routes.store_users_route import store_users_bp
 import os
-
 
 PORT = os.getenv('PORT')
 HOST = os.getenv('HOST')
 DEBUG = os.getenv('DEBUG')
 
 app = Flask(__name__)
+spec = FlaskPydanticSpec('Flask', titulo='API - NORTE AUTO PEÇAS')
+spec.register(app)
 
 #chamada das blueprints
 app.register_blueprint(users_bp)
+app.register_blueprint(store_users_bp)
 
 app.run(port=PORT, host=HOST, debug=True, load_dotenv=True, use_reloader=True)
