287440c436c6a0f5e92dec7cbda296770993015f
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-12-18 13:19:30 -0400

    feat: criando e validando as rotas de usuario.

--- server/routes/users_route.py
+++ server/routes/users_route.py
@@ -1,31 +1,41 @@
 from flask import Blueprint, request, jsonify
 from infra.controllers.users_controller import UsersResponse
+from flask_pydantic_spec import FlaskPydanticSpec # Cria um endpoint chamado /doc/swagger que mostra os endpoints que tenho na minha aplicação.
 
-# Criando o Blueprint para usuários
 users_bp = Blueprint('users', __name__)
 
 response_users = UsersResponse()
 
-# Rota GET para todos os usuários
+spec = FlaskPydanticSpec('Flask', titulo='API - NORTE AUTO PEÇAS')
+spec.register(users_bp)
+
+# Rota GET por Nome e Senha
 @users_bp.route('/usuario', methods=['GET'])
-def get_users():
+def get_users_by_name_and_pass():
+    req_user = request.get_json()
+
+    name = req_user.get('name')
+    password = req_user.get('password')
+
+    if not name or not password:
+        return jsonify({'error': 'Nome e senha são obrigatórios!'}), 400
+
+    get_user = {
+        "name": name,
+        "password": password
+    }
+
     data = response_users.select()
-    data_to_dict = [user.to_dict() for user in data]
-    return jsonify(data_to_dict)
 
-
-
-# Rota GET por ID
-@users_bp.route('/usuario/<int:id>', methods=['GET'])
-def get_users_by_id(id):
-    data = response_users.select()
     data_to_dict = [user.to_dict() for user in data]
 
-    for i, users in enumerate(data_to_dict):
-        if users['id'] == id:
-            return jsonify(users)
-
-
+    if get_user['name'] and get_user['password']:
+        for i, users in enumerate(data_to_dict):
+            if users['name'] == get_user['name'] and users['password'] == get_user['password']:
+                return jsonify(users), 200
+        else:
+            return jsonify({'error': 'informação invalidas ou vazias, favor, verifique seu cadastro!'}), 400
+    
 
 # Rota POST para inserir usuário
 @users_bp.route('/usuario', methods=['POST'])
@@ -33,8 +43,11 @@ def insert_user():
     try:
         new_user = request.get_json()
         id = new_user.get('id')
-        nome = new_user.get('nome')
+        nome = new_user.get('name')
         password = new_user.get('password')
+
+        if not nome or not password or not id:
+            return jsonify({'error': 'Nome e senha são obrigatórios!'}), 400
 
         if id and nome and password:
             response_users.insert(nome, password, id)
@@ -46,7 +59,6 @@ def insert_user():
     except Exception as e:
         return jsonify({"error": f"Ocorreu um erro interno: {e}"}), 500
     
-
 
 # Rota DELETE por ID
 @users_bp.route('/usuario/<int:id>', methods=['DELETE'])
@@ -61,7 +73,7 @@ def delete_user_by_id(id):
 def update_user(id):
     try:
         updated_user = request.get_json()
-        nome = updated_user.get('nome')
+        nome = updated_user.get('name')
         password = updated_user.get('password')
 
         if nome and password:
