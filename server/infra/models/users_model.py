ec49dace5e4ade55c0230657be210acc730191ca
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-12-19 14:01:25 -0400

    feat: adicionando as regras para cada usuario

--- server/infra/models/users_model.py
+++ server/infra/models/users_model.py
@@ -13,13 +13,15 @@ class Users(Base):
     ID = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     NAME = Column(String, nullable=False)
     PASSWORD = Column(String, nullable=False)
+    ROLE = Column(String)
 
     def __repr__(self):
-        return f"[Id={self.ID}, Name='{self.NAME}', Password='{self.PASSWORD}']"
+        return f"[Id={self.ID}, Name='{self.NAME}', Password='{self.PASSWORD}, Role='{self.ROLE}']"
 
     def to_dict(self):
         return {
             "id": self.ID,
             "name": self.NAME,
             "password": self.PASSWORD,
+            "role": self.ROLE
         }
