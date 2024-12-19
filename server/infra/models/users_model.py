783a59ec5e4dbad5bdeceb58c13fbb698017f163
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-12-19 17:57:39 -0400

    style: alterando nome da tabela de users para D_USERS

--- server/infra/models/users_model.py
+++ server/infra/models/users_model.py
@@ -8,7 +8,7 @@ from configs.base import Base
 from sqlalchemy import Column, String, Integer
 
 class Users(Base):
-    __tablename__ = 'users'
+    __tablename__ = 'D_USERS'
 
     ID = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     NAME = Column(String, nullable=False)
