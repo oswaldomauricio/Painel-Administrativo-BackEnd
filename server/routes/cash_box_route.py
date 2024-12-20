4517e97771d411d855330e558eb572aebb16bd99
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-12-20 17:17:19 -0400

    fix: adicionando o controller do select na rota

--- server/routes/cash_box_route.py
+++ server/routes/cash_box_route.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, request, jsonify
 from infra.controllers.cash_box_controller import ResponseCashBox
+from infra.controllers.cash_box_controller import CashBoxController
+from infra.controllers.cash_box_controller import ResponseCashBox
 from flask_pydantic_spec import FlaskPydanticSpec # Cria um endpoint chamado /doc/swagger que mostra os endpoints que tenho na minha aplicação.
-
 cashbox_bp = Blueprint('cash box', __name__)
-
-response_cashbox = ResponseCashBox()
 
 spec = FlaskPydanticSpec('Flask', titulo='API - NORTE AUTO PEÇAS')
 spec.register(cashbox_bp)
+
+response_cashBox = ResponseCashBox()
+
+# Instancie o controlador
+CashBox_Controller = CashBoxController(response_cashBox)
 
 # Rota GET por Nome e Senha
 @cashbox_bp.route('/cashbox', methods=['GET'])
@@ -25,16 +29,7 @@ def get_cashbox_by_store_and_tipo_operacao():
         "tipo_operacao": tipo_operacao
     }
 
-    data = response_cashbox.select()
-
-    data_to_dict = [infos.to_dict() for infos in data]
-
-    return jsonify(data_to_dict)
-
-    # if get_info['name'] and get_user['password']:
-    #     for i, users in enumerate(data_to_dict):
-    #         if users['name'] == get_user['name'] and users['password'] == get_user['password']:
-    #             return jsonify(users), 200
-    #     else:
-    #         return jsonify({'error': 'informação invalidas ou vazias, favor, verifique seu cadastro!'}), 400
+    result = CashBox_Controller.select_all_info_by_cashBox(get_info['id_loja'], get_info['tipo_operacao'])
+    print(result.get_json())  # Para extrair os dados JSON
+    return result.get_json()
     
