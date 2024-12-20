a8e036be3a0d5142abcd8df6531c448b674bfe28
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-12-20 09:22:25 -0400

    style: adicionando o blueprint de cashbox ao app para ter acesso as rotas

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_pydantic_spec import FlaskPydanticSpec # Cria um endpoint chamado /doc/swagger que mostra os endpoints que tenho na minha aplicação.
 from routes.users_route import users_bp
 from routes.store_users_route import store_users_bp
+from routes.cash_box_route import cashbox_bp
 import os
 
 PORT = os.getenv('PORT')
@@ -15,5 +16,7 @@ spec.register(app)
 #chamada das blueprints
 app.register_blueprint(users_bp)
 app.register_blueprint(store_users_bp)
+app.register_blueprint(cashbox_bp)
+
 
 app.run(port=PORT, host=HOST, debug=True, load_dotenv=True, use_reloader=True)
