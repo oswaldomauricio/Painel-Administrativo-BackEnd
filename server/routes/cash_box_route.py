915a09ee68ae7ca558820965116ac0eda84bc588
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2024-12-23 15:17:17 -0400

    fix: trocando o filtro de tipo_operacao para data_operacao para filtrar somente pela data e loja

--- server/routes/cash_box_route.py
+++ server/routes/cash_box_route.py
@@ -2,7 +2,7 @@ from flask import Blueprint, request, jsonify
 from infra.controllers.cash_box_controller import ResponseCashBox
 from infra.controllers.cash_box_controller import CashBoxController
 from infra.controllers.cash_box_controller import ResponseCashBox
-from flask_pydantic_spec import FlaskPydanticSpec # Cria um endpoint chamado /doc/swagger que mostra os endpoints que tenho na minha aplicação.
+from flask_pydantic_spec import FlaskPydanticSpec # type: ignore # Cria um endpoint chamado /doc/swagger que mostra os endpoints que tenho na minha aplicação.
 cashbox_bp = Blueprint('cash box', __name__)
 
 spec = FlaskPydanticSpec('Flask', titulo='API - NORTE AUTO PEÇAS')
@@ -19,17 +19,17 @@ def get_cashbox_by_store_and_tipo_operacao():
     req_cashbox = request.get_json()
 
     id_loja = req_cashbox.get('id_loja')
-    tipo_operacao = req_cashbox.get('tipo_operacao')
+    date_operacao = req_cashbox.get('data')
 
-    if not id_loja or not tipo_operacao:
-        return jsonify({'error': 'loja e o tipo de operação não informado!'}), 400
+    if not id_loja and not date_operacao:
+        return jsonify({'error': 'loja e a data não foram informados, favor verificar!'}), 400
 
     get_info = {
         "id_loja": id_loja,
-        "tipo_operacao": tipo_operacao
+        "date": date_operacao
     }
 
-    result = CashBox_Controller.select_all_info_by_cashBox(get_info['id_loja'], get_info['tipo_operacao'])
-    print(result.get_json())  # Para extrair os dados JSON
+    result = CashBox_Controller.select_all_info_by_cashBox(get_info['id_loja'], get_info['date'])
+    print(result.get_json())
     return result.get_json()
     
