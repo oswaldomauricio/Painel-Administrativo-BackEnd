57aced00202db32d99c2d4f47c5d30baf6b83cd2
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-01-30 15:30:17 -0400

    fix: ajustando o db_connection e criando uma instancia para nao executar mais de uma vez no banco.

--- server/infra/configs/connection.py
+++ server/infra/configs/connection.py
@@ -4,23 +4,36 @@ import cx_Oracle
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-load_dotenv() 
-
-PORTBD = os.getenv('PORTBD')
-HOSTBD = os.getenv('HOSTBD')
-SERVICENAME = os.getenv('SERVICENAME')
-USER = os.getenv('USER')
-PASSWORD = os.getenv('PASSWORD')
-
-cx_Oracle.init_oracle_client(
-    lib_dir=r'C:\instantclient-basic-windows.x64-23.6.0.24.10\instantclient_23_6')
-
-
 class DBconnection:
+    _instance = None
+    _initialized = False
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(DBconnection, cls).__new__(cls)
+        return cls._instance
+
     def __init__(self):
-        self.__connection_string = f'oracle+cx_oracle://{USER}:{PASSWORD}@{HOSTBD}:{PORTBD}/?service_name={SERVICENAME}'
-        self.__engine = self.__create_database_engine()
-        self.session = None
+        if not DBconnection._initialized:
+            load_dotenv()
+            
+            self.PORTBD = os.getenv('PORTBD')
+            self.HOSTBD = os.getenv('HOSTBD')
+            self.SERVICENAME = os.getenv('SERVICENAME')
+            self.USER = os.getenv('USER')
+            self.PASSWORD = os.getenv('PASSWORD')
+
+            try:
+                cx_Oracle.init_oracle_client(
+                    lib_dir=r'C:\instantclient-basic-windows.x64-23.6.0.24.10\instantclient_23_6'
+                )
+            except Exception as e:
+                pass
+
+            self.__connection_string = f'oracle+cx_oracle://{self.USER}:{self.PASSWORD}@{self.HOSTBD}:{self.PORTBD}/?service_name={self.SERVICENAME}'
+            self.__engine = self.__create_database_engine()
+            self.session = None
+            DBconnection._initialized = True
 
     def __create_database_engine(self):
         engine = create_engine(self.__connection_string)
@@ -37,3 +50,6 @@ class DBconnection:
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.session:
             self.session.close()
+
+# Criar uma instância global para ser usada em toda a aplicação
+db_instance = DBconnection()
