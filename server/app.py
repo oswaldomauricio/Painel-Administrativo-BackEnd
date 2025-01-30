ccb61703e429f110b66417c0b07dad0efccbcfcf
oswaldomauricio <37278259+oswaldomauricio@users.noreply.github.com>
2025-01-30 15:32:18 -0400

    feat: adicionando a rota de lojas no app

--- server/app.py
+++ server/app.py
@@ -1,8 +1,9 @@
 from flask import Flask
-from flask_pydantic_spec import FlaskPydanticSpec # Cria um endpoint chamado /doc/swagger que mostra os endpoints que tenho na minha aplicação.
+from flask_pydantic_spec import FlaskPydanticSpec
 from routes.users_route import users_bp
 from routes.store_users_route import store_users_bp
 from routes.cash_box_route import cashbox_bp
+from routes.metas.meta_lojas_routes import meta_lojas_bp 
 import os
 
 PORT = os.getenv('PORT')
@@ -10,13 +11,14 @@ HOST = os.getenv('HOST')
 DEBUG = os.getenv('DEBUG')
 
 app = Flask(__name__)
-spec = FlaskPydanticSpec('Flask', titulo='API - NORTE AUTO PEÇAS')
+spec = FlaskPydanticSpec('Flask', title='API - NORTE AUTO PEÇAS')
 spec.register(app)
 
 #chamada das blueprints
 app.register_blueprint(users_bp)
 app.register_blueprint(store_users_bp)
 app.register_blueprint(cashbox_bp)
+app.register_blueprint(meta_lojas_bp)
 
 
 app.run(port=PORT, host=HOST, debug=True, load_dotenv=True, use_reloader=True)
